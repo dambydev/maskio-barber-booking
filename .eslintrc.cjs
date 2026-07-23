956b1c4dad464bd17eaf3d5ac7d05d7a4dc7a933
dambystudio <davide431@outlook.it>
2026-07-23 22:08:52 +0200

    fix(deps): update dependencies, resolve security vulnerabilities and update eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prefer-const": "off",
+    "@next/next/no-sync-scripts": "off"
+  }
+};
+
+
